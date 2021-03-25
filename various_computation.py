babaca4edb591dd22878205f398776f235c5e972
hyrumk <hyrumk@kaist.ac.kr>
2021-03-25 22:26:01 +0900

    Add entropy equation

---
+++ various_computation.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+#entropy
+def entropy(X):
+    '''
+
+    :param X: (numpy) Input
+    :return: (numpy) Entropy of the input
+    '''
+    unique, count = np.unique(X, return_counts=True, axis=0)
+    prob = count/len(X)
+    en = np.sum((-1)*prob*np.log2(prob))
+    return en
+
+#Joint Entropy
+def jEntropy(Y,X):
+    """
+    H(Y;X)
+    Reference: https://en.wikipedia.org/wiki/Joint_entropy
+    """
+    YX = np.c_[Y,X]
+    return entropy(YX)
+
+#Conditional Entropy
+def cEntropy(Y, X):
+    """
+    conditional entropy = Joint Entropy - Entropy of X
+    H(Y|X) = H(Y;X) - H(X)
+    Reference: https://en.wikipedia.org/wiki/Conditional_entropy
+    """
+    return jEntropy(Y, X) - entropy(X)
+
+a = np.random.rand(5,5)
+b = np.random.rand(5,5)
+print(cEntropy(a, a))
