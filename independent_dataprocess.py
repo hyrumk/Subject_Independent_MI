ac572a51aef7956b12273be2755f1823110833dd
hyrumk <hyrumk@kaist.ac.kr>
2021-04-07 12:53:59 +0900

    working on train function

--- independent_dataprocess.py
+++ independent_dataprocess.py
@@ -5,7 +5,7 @@ import copy
 import numpy as np
 from mne.decoding import CSP
 from braindecode.datautil.windowers import create_windows_from_events
-import mne
+import torch
 from various_computation import computeMI
 
 frequency_bands = [(7.5,14),(11,13),(10,14),(9,12),(19,22),(16,22),(26,34),(17.5,20.5),(7,30),
@@ -106,12 +106,18 @@ def generate_ss_feature(dataset, num_selected_bands=20):
     :param dataset: (list) a list of MOABBDatasets by subject (directly from return of call_data)
     :param num_channels:
     :param num_selected_bands:
-    :return:
+    :return: (list[list[Tensor] (X), Y, frequency_range_order (for test dataset use))
     '''
+    frequency_bands = [(7.5, 14), (11, 13), (10, 14), (9, 12), (19, 22), (16, 22), (26, 34), (17.5, 20.5), (7, 30),
+                       (5, 14), (11, 31), (12, 18), (7, 9), (15, 17), (25, 30), (20, 25), (5, 10), (10, 25), (15, 30),
+                       (10, 12), (23, 27), (28, 32), (12, 33), (11, 22), (5, 8), (7.5, 17.5), (23, 26), (5, 20),
+                       (5, 25), (10, 20)]
+
     filtered_dataset = []   # will be a list of (mutual_info, filtered data -> (X,Y) from the for loop, frequency band)
     num_trials = num_subjects = N_COMPONENTS = 0
 
     for k,filter_range in enumerate(frequency_bands):
+        #<TODO> Copy the entire dataset so that it doesn't perform bandpass filter on the same data (IF THAT's the case)
         kth_filtered = bandpass_data(dataset, filter_range) # [window_subj1, window_subj2 ... window_subjN]
                                                             # Each window_subj1 consisted of (x,y,window_ind) by trial
         #num_trials =  len(kth_filtered[0])# trial/subject
@@ -127,6 +133,7 @@ def generate_ss_feature(dataset, num_selected_bands=20):
         X = np.concatenate(list_X)  # concatenated X from all the subjects bandpass filtered by current filter range
         Y = np.concatenate(list_Y)  # concatenated X from all the subjects bandpass filtered by current filter range
 
+        print("X first: ", X[0])
 
         N_COMPONENTS = len(np.unique(Y)) if N_COMPONENTS == 0 else N_COMPONENTS
         csp = CSP(n_components=N_COMPONENTS, reg=None, log=True, norm_trace=False)
@@ -137,7 +144,7 @@ def generate_ss_feature(dataset, num_selected_bands=20):
         Wk = np.column_stack((W[:,:U], W[:,-U:])) # first U and last U columns from the filter (U = num_selected_bands/2)
         Vk = [] # where v will be stored
 
-        for x in X: # X.shape[0] == num_subjects*num_trials
+        for x in X: # X.shape[0] == num_subjects*num_trials # Iterate through X by trial
             mat = Wk.T @ x
             v = np.log(np.var(mat))
             Vk.append(v)
@@ -145,22 +152,101 @@ def generate_ss_feature(dataset, num_selected_bands=20):
         Vk = np.array(Vk)
         mutual_info = computeMI(Vk, Y)
 
-        filtered_dataset.append((mutual_info, (X,Y), filter_range))
+        filtered_dataset.append((mutual_info, (X,Y), Wk, filter_range, Vk))
 
-    return filtered_dataset
-        #<TODO>
+    #<TODO> sort filtered_dataset by mutual_info in descending order
+    filtered_dataset.sort() # sort by mutual_info.
+    C_list = []
+    Y = filtered_dataset[0][1][2]
+    for tup in filtered_dataset[:num_selected_bands]:
+        X = tup[1][0]
+        Wk = tup[2]
+
+        C = []
+        for x in X:
+            mult = Wk.T @ x
+            cip = np.cov(mult)
+            C.append(torch.Tensor(cip))
+        #C = np.concatenate(C)
+        C_list.append(C)
+    # C_list will contain 20 different list (C1~C20) which includes spectral spatial inputs of each of 20 frequency ranges
+    input_list = [list(i) for i in zip(*C_list)]
+
+    frequency_range_order = [tup[3] for tup in filtered_dataset]
+
+    #return filtered_dataset
+        #<TODO> Fix computeMI
+    return input_list, Y, frequency_range_order
+
+#<TODO> Need to gather 20 spectral inputs into one.
+
+def generate_ss_feature_test(dataset, frequency_range_order):
+
+
+    filtered_dataset = []
+
+    for k,filter_range in enumerate(frequency_range_order):
+        kth_filtered = bandpass_data(dataset, filter_range) # [window_subj1, window_subj2 ... window_subjN]
+                                                            # Each window_subj1 consisted of (x,y,window_ind) by trial
+        #num_trials =  len(kth_filtered[0])# trial/subject
+        #num_subjects = len(kth_filtered)
+        #trial_time_sample = kth_filtered[0][0][0].shape[1]
+        U = len(frequency_range_order)//2
+
+        list_XY = [windows_to_XY(subj_windows) for subj_windows in kth_filtered]
+        list_X = [subj_data[0] for subj_data in list_XY]
+        list_Y = [subj_data[1] for subj_data in list_XY]
+
+        #<TODO> possible necessary change in X shape currently (data #, channel, time point) -> (data #, time point, channel) CHANGE IT IF RESULT GETS WEIRD
+        X = np.concatenate(list_X)  # concatenated X from all the subjects bandpass filtered by current filter range
+        Y = np.concatenate(list_Y)  # concatenated X from all the subjects bandpass filtered by current filter range
+
+        print("X first: ", X[0])
+
+        N_COMPONENTS = len(np.unique(Y)) if N_COMPONENTS == 0 else N_COMPONENTS
+        csp = CSP(n_components=N_COMPONENTS, reg=None, log=True, norm_trace=False)
+        csp_fit = csp.fit_transform(X, Y)
+
+        # <TODO> MIGHT NEED TO COMPUTE MUTUAL INFO ON OUR OWN
+        W = csp.filters_
+        Wk = np.column_stack((W[:,:U], W[:,-U:])) # first U and last U columns from the filter (U = num_selected_bands/2)
+        filtered_dataset.append((X,Y), Wk)
+
+    C_list = []
+    for tup in filtered_dataset:
+        X = tup[0][0]
+        Wk = tup[1]
+
+        C = []
+        for x in X:
+            mult = Wk.T @ x
+            cip = np.cov(mult)
+            C.append(torch.Tensor(cip))
+        # C = np.concatenate(C)
+        C_list.append(C)
+        # C_list will contain 20 different list (C1~C20) which includes spectral spatial inputs of each of 20 frequency ranges
+    input_list = [list(i) for i in zip(*C_list)] #X
+    Y = filtered_dataset[0][0][1]                #Y
+
+    return input_list, Y
+
+
+
 
 
 DATASET_NAME = "BNCI2014001"
-data = call_data(DATASET_NAME, [1,2,3])#[i for i in range(1,10)])
-filt_dataset = generate_ss_feature(data)
+data = call_data(DATASET_NAME, [1,2])#[i for i in range(1,10)])
+X, Y, frequency_order  = generate_ss_feature(data)
 
-print(len(filt_dataset)) # should have 30
-print(len(filt_dataset[0]))
-print(filt_dataset[0][0])
-print("mutual info list:  ",[tup[0] for tup in filt_dataset])
-print("X,Y shape list: ", [(tup[1][0].shape, tup[1][1].shape) for tup in filt_dataset])
-print("filter range list: ", [tup[2] for tup in filt_dataset])
+print(frequency_order)
+print(len(X))
+print(len(X[0])) # should be 20
+print(X[0][0].shape) # spectral input
+print(Y.shape)
+#print("mutual info list:  ",[tup[0] for tup in filt_dataset])
+#print("X,Y shape list: ", [(tup[1][0].shape, tup[1][1].shape) for tup in filt_dataset])
+#print("X list: ", [tup[1][0][0] for tup in filt_dataset])
+#print("Wk list: ", [tup[2] for tup in filt_dataset])
 
 
 
--- spectralNet.py
+++ spectralNet.py
@@ -2,6 +2,9 @@ import numpy as np
 import torch
 from torch import nn, cat
 from torch.nn import init
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
 
 from braindecode.util import np_to_var
 from braindecode.models.modules import Expression, Ensure4d
@@ -52,13 +55,55 @@ class SpectralNet(nn.Module):
     def forward(self, input_list):
         '''
 
-        :param input_list: (list) a list of spectral input tensors
+        :param input_list: (list[Tensor]) a list of spectral input tensors
         :return:
         '''
 
-        concat_fusion = cat([cnn(x) for x,cnn in zip(input_list,self.features)])
+        concat_fusion = cat([cnn(x) for x,cnn in zip(input_list,self.features)], dim = 0)
         output = self.fc_module(concat_fusion)
         return output
+
+
+
+#def spectral_input_to_dataloader():
+
+def numpy_to_trainloader(X,Y, batch_size, num_workers = 4):
+    tensor_x = torch.Tensor(X)
+    tensor_y = torch.Tensor(Y)
+    dataset = TensorDataset(tensor_x, tensor_y)
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True,
+                                               num_workers=num_workers)
+
+
+
+def train(epoch, train_loader, model, optimizer, batch_size = 100):
+
+    use_cuda = torch.cuda.is_available()
+
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if use_cuda:
+            data = data.cuda()
+            target = target.cuda()
+        optimizer.zero_grad() # set the gradients to zero before starting backpropragation
+        output = model(data) # model prediction
+        loss = F.nll_loss(output, target) # calculate negative log likelihood
+        loss.backward() # calculate gradients
+        optimizer.step() # update gradients
+        if batch_idx % 100 == 0: # print train loss per every 100 batch_idx.
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def train_and_test(train_loader, epoch, test_loader):
+    use_cuda = torch.cuda.is_available()
+    model = SpectralNet(20).cuda() if use_cuda else SpectralNet(20)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+    train(epoch, train_loader, model, optimizer)
+
+# <TODO> finish train, train_and_test, numpy_to_trainloader
+
 
 
 #summary(SpectralNet, (28,28,20))
