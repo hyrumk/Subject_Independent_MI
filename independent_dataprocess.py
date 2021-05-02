Fixed formula in spectral input generating

independent_dataprocess.py
@@ -7,7 +7,7 @@ from mne.decoding import CSP
 from braindecode.datautil.windowers import create_windows_from_events
 import torch
 from various_computation import computeMI
-from sklearn import metrics
+from sklearn import feature_selection
 
 frequency_bands = [(7.5,14),(11,13),(10,14),(9,12),(19,22),(16,22),(26,34),(17.5,20.5),(7,30),
                    (5,14),(11,31),(12,18),(7,9),(15,17),(25,30),(20,25),(5,10),(10,25),(15,30),
@@ -104,12 +104,12 @@ def windows_to_XY(windows_dataset):
     return X,Y
 
 
-def generate_ss_feature(dataset, num_sp=22):
+def generate_ss_feature(dataset, num_components=22, num_band = 20):
     '''
 
     :param dataset: (list) a list of MOABBDatasets by subject (directly from return of call_data)
-    :param num_channels:
-    :param num_sp: number of spatial filters to be obtained from the CSP algorithm
+    :param num_components: number of components to use from spatial filters
+    :param num_band: number of spatial filters to be obtained from the CSP algorithm
     :return: (list[list[Tensor] (X), Y, frequency_range_order (for test dataset use))
     '''
     frequency_bands = [(7.5, 14), (11, 13), (10, 14), (9, 12), (19, 22), (16, 22), (26, 34), (17.5, 20.5), (7, 30),
@@ -118,30 +118,22 @@ def generate_ss_feature(dataset, num_sp=22):
                        (5, 25), (10, 20)]
 
     filtered_dataset = []   # will be a list of (mutual_info, filtered data -> (X,Y) from the for loop, frequency band)
-    num_trials, num_subjects, N_COMPONENTS = 0,0,0
-    V_list = []
+    N_COMPONENTS = num_components
+
     for k,filter_range in enumerate(frequency_bands):
         #<TODO> Copy the entire dataset so that it doesn't perform bandpass filter on the same data (IF THAT's the case)
         kth_filtered = bandpass_data(dataset, filter_range) # [window_subj1, window_subj2 ... window_subjN]
                                                             # Each window_subj1 consisted of (x,y,window_ind) by trial
-        #num_trials =  len(kth_filtered[0])# trial/subject
-        #num_subjects = len(kth_filtered)
-        #trial_time_sample = kth_filtered[0][0][0].shape[1]
-        U = num_sp//2
-
         list_XY = [windows_to_XY(subj_windows) for subj_windows in kth_filtered]
         list_X = [subj_data[0] for subj_data in list_XY]
         list_Y = [subj_data[1] for subj_data in list_XY]
-
-        print(len(list_X))
-        print(len(list_Y))
 
         #<TODO> possible necessary change in X shape currently (data #, channel, time point) -> (data #, time point, channel) CHANGE IT IF RESULT GETS WEIRD
         X = np.concatenate(list_X)  # concatenated X from all the subjects bandpass filtered by current filter range
         Y = np.concatenate(list_Y)  # concatenated X from all the subjects bandpass filtered by current filter range
 
         '''
-        ## This is where we trim down the label into binary // DELETE IF NEEDED
+        ## This is where we can trim down the label into binary // DELETE IF NEEDED
         new_X = []
         new_Y = []
         for i, y_elem in enumerate(Y):
@@ -156,70 +148,51 @@ def generate_ss_feature(dataset, num_sp=22):
         ## Up to here
         '''
 
-        N_COMPONENTS = len(np.unique(Y)) if N_COMPONENTS == 0 else N_COMPONENTS
-        csp = CSP(n_components=N_COMPONENTS, reg=None, log=True, norm_trace=False)
+        #N_COMPONENTS = len(np.unique(Y)) if N_COMPONENTS == 0 else N_COMPONENTS
+        csp = CSP(n_components=N_COMPONENTS, reg=None, log=True, norm_trace=False, component_order='mutual_info' )
         csp_fit = csp.fit_transform(X, Y)
-        print(csp_fit.shape)
+        # <TODO> compare Vk and csp_fit mutual_info
+        Wk = csp.filters_
 
-        # <TODO> MIGHT NEED TO COMPUTE MUTUAL INFO ON OUR OWN
-        W = csp.filters_
-        Wk = np.column_stack((W[:,:U], W[:,-U:])) # first U and last U columns from the filter (U = num_selected_bands/2)
-        Vk = [] # where v will be stored
-
-        a = 0
-        for x in X: # X.shape[0] == num_subjects*num_trials # Iterate through X by trial
-            mat = Wk.T @ x
-            print(mat.shape)
-            exit(0)
-            v = np.log(np.var(mat))
-            Vk.append(v)
-
-        Vk = np.array(Vk)
         #mutual_info = computeMI(Vk, Y)
-        mutual_info = metrics.mutual_info_score(Y, Vk)
-        V_list.append(Vk)
+        mutual_info = np.amax(feature_selection.mutual_info_classif(csp_fit,Y))
+        #mutual_info = feature_selection.mutual_info_classif(Vk, Y)
 
-        filtered_dataset.append((mutual_info, (X,Y), Wk, filter_range, Vk))
+        filtered_dataset.append((mutual_info, (X,Y), Wk, filter_range))
 
-    mi = [tup[0] for tup in filtered_dataset]
-    print("MUTUAL INFO!!: ", mi)
-    print("LENGTH: ", len(V_list[0]))
-    print("Vk: ", V_list[0])
-    print("Vk2: ", V_list[1])
-    print("Vk3: ", V_list[2])
-    print("Vk4: ", V_list[3])
-    print("Vk5: ", V_list[4])
-    print("Vk6: ", V_list[5])
-    print("Y: ", Y)
-    exit(0)
-    #<TODO> sort filtered_dataset by mutual_info in descending order
-    filtered_dataset.sort(key=lambda tup: tup[0]) # sort by mutual_info.
+    filtered_dataset.sort(key=lambda tup: tup[0], reverse=True) # sort by mutual_info.
+
 
     C_list = []
     Y = filtered_dataset[0][1][1]
-    for tup in filtered_dataset[:num_sp]:
+    for tup in filtered_dataset[:num_band]:
         X = tup[1][0]
-        Wk = tup[2]
-        C = []
+        Wk = tup[2][:N_COMPONENTS]
+
+        '''
+        C=[]
         for x in X:
-            mult = Wk.T @ x
+            print(Wk.shape)
+            print(x.shape)
+            exit(0)
+            mult = Wk @ x
             cip = np.cov(mult)
             C.append(cip) #torch.Tensor(cip)
+        '''
+        C = [np.cov(Wk@x) for x in X]
         #C = np.concatenate(C)
         C_list.append(C)
     # C_list will contain 20 different list (C1~C20) which includes spectral spatial inputs of each of 20 frequency ranges
     # C_list = list(20*list(spectral spatial inputs for each trial))
-    input_list = [list(i) for i in zip(*C_list)]
 
+    input_list = [list(i) for i in zip(*C_list)]
     frequency_range_order = [tup[3] for tup in filtered_dataset]
 
-    #return filtered_dataset
-        #<TODO> Fix computeMI
     return input_list, Y, frequency_range_order
 
-#<TODO> Need to gather 20 spectral inputs into one.
+#<TODO> Need to gather 20 spectral inputs into one. Fix according to generate_ss_feature
 
-def generate_ss_feature_test(dataset, frequency_range_order):
+def generate_ss_feature_test(dataset, frequency_range_order, N_COMPONENTS = 22):
     '''
 
     :param dataset: (list[MOABBDataset])
@@ -232,10 +205,6 @@ def generate_ss_feature_test(dataset, frequency_range_order):
     for k,filter_range in enumerate(frequency_range_order):
         kth_filtered = bandpass_data(dataset, filter_range) # [window_subj1, window_subj2 ... window_subjN]
                                                             # Each window_subj1 consisted of (x,y,window_ind) by trial
-        #num_trials =  len(kth_filtered[0])# trial/subject
-        #num_subjects = len(kth_filtered)
-        #trial_time_sample = kth_filtered[0][0][0].shape[1]
-        U = len(frequency_range_order)//2
 
         list_XY = [windows_to_XY(subj_windows) for subj_windows in kth_filtered]
         list_X = [subj_data[0] for subj_data in list_XY]
@@ -245,15 +214,11 @@ def generate_ss_feature_test(dataset, frequency_range_order):
         X = np.concatenate(list_X)  # concatenated X from all the subjects bandpass filtered by current filter range
         Y = np.concatenate(list_Y)  # concatenated X from all the subjects bandpass filtered by current filter range
 
-        print("X first: ", X[0])
-
-        N_COMPONENTS = len(np.unique(Y)) if N_COMPONENTS == 0 else N_COMPONENTS
         csp = CSP(n_components=N_COMPONENTS, reg=None, log=True, norm_trace=False)
         csp_fit = csp.fit_transform(X, Y)
 
-        # <TODO> MIGHT NEED TO COMPUTE MUTUAL INFO ON OUR OWN
-        W = csp.filters_
-        Wk = np.column_stack((W[:,:U], W[:,-U:])) # first U and last U columns from the filter (U = num_selected_bands/2)
+        Wk = csp.filters_
+
         filtered_dataset.append(((X,Y), Wk))
 
     C_list = []
@@ -261,12 +226,8 @@ def generate_ss_feature_test(dataset, frequency_range_order):
         X = tup[0][0]
         Wk = tup[1]
 
-        C = []
-        for x in X:
-            mult = Wk.T @ x
-            cip = np.cov(mult)
-            C.append(cip)
-        # C = np.concatenate(C)
+        C = [np.cov(Wk@x) for x in X]
+
         C_list.append(C)
         # C_list will contain 20 different list (C1~C20) which includes spectral spatial inputs of each of 20 frequency ranges
     input_list = [list(i) for i in zip(*C_list)] #X

main.py
@@ -15,24 +15,7 @@ if __name__ == '__main__':
     DATASET_NAME = "BNCI2014001"
     #data_list = call_data(DATASET_NAME, [i for i in range(1,10)])
     #data = call_data(DATASET_NAME, [i for i in range(1,10)])
-    '''    #Calls the data and preprocess
-    data = call_data(DATASET_NAME, [1,2])
-    i = 0
-    subject_data = data[i]
-    X, Y, frequency_order = generate_ss_feature(data)
-    print(frequency_order)
-    print(len(X)) # number of trials
-    print(len(X[0]))  # should be 20
-    print(X[0][0].shape)  # a single spectral input
-    print(Y.shape)
-    
-    
-    # for saving in a pkl file
-    with open('data_X.pkl','wb') as f:
-        pickle.dump(X, f)
-    with open('data_Y.pkl', 'wb') as f:
-        pickle.dump(Y, f)
-    '''
+
     data = []
     data_test = []
     CHOSEN_TEST_SUBJECT = 9
@@ -47,20 +30,10 @@ if __name__ == '__main__':
             subject_data = pickle.load(f)
             data.append(subject_data)
 
-    X,Y,frequency_order = generate_ss_feature(data_test)
 
+    #Where you use the data to make a filtered data
+    #X, Y, frequency_order = generate_ss_feature(data) # train data
 
-    ''' Where you use the data to make a filtered data
-    X, Y, frequency_order = generate_ss_feature(data) # train data
-
-    # Saving part
-    with open('data_train_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'wb') as f:
-        pickle.dump(X, f)
-    with open('data_train_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT), 'wb') as f:
-        pickle.dump(Y, f)
-    with open('data_freq_order{}.pkl'.format(CHOSEN_TEST_SUBJECT),'wb') as f:
-        pickle.dump(frequency_order, f)
-    '''
     with open('data_train_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
         X = pickle.load(f)
     with open('data_train_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
@@ -68,35 +41,56 @@ if __name__ == '__main__':
     with open('data_freq_order{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
         frequency_order = pickle.load(f)
 
-    #X_test, Y_test = generate_ss_feature_test(data_test, frequency_order[:20])
+
+    X_test, Y_test = generate_ss_feature_test(data_test, frequency_order[:20])  # test data
+    with open('data_test_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'wb') as f:
+        pickle.dump(X_test, f)
+    with open('data_test_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT), 'wb') as f:
+        pickle.dump(Y_test, f)
 
     with open('data_test_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
         X_test = pickle.load(f)
     with open('data_test_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
         Y_test = pickle.load(f)
 
+
     # X_train, Y_train, frequency_order = generate_ss_feature(data[:-1])
     # X_test, Y_test = generate_ss_feature_test(data[-1], frequency_order)
     X_train = np.array(X)
-    print(X_train[0][0])
-    print(X_train[0][1])
-    print(X_train[1][0])
-    print(X_train[1][1])
-    exit(0)
+
+
     #X_test = np.array(X_test)
     trainloader = sn.numpy_to_trainloader(X_train,Y,100)
     testloader = sn.numpy_to_trainloader(X_test, Y_test, 100)
-    sn.train_and_test(trainloader, 20, testloader)
+    sn.train_and_test(trainloader, 200, testloader)
 
 '''
+
     This brings pickled data (from generate_ss_feature)
     with open('data_X.pkl', 'rb') as f:
         X = pickle.load(f)
     with open('data_Y.pkl', 'rb') as f:
         Y = pickle.load(f)
+    
+    
+    #### FOR LOADING preprocessed TRAINING DATA#####  
+    with open('data_train_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
+        X = pickle.load(f)
+    with open('data_train_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
+        Y = pickle.load(f)
+    with open('data_freq_order{}.pkl'.format(CHOSEN_TEST_SUBJECT),'rb') as f:
+        frequency_order = pickle.load(f)
+    ###################################################
 
-
-
+####################################
+    # Saving part
+    with open('data_train_X{}.pkl'.format(CHOSEN_TEST_SUBJECT),'wb') as f:
+        pickle.dump(X, f)
+    with open('data_train_Y{}.pkl'.format(CHOSEN_TEST_SUBJECT), 'wb') as f:
+        pickle.dump(Y, f)
+    with open('data_freq_order{}.pkl'.format(CHOSEN_TEST_SUBJECT),'wb') as f:
+        pickle.dump(frequency_order, f)
+##############################################
     #run_sample_model(subject_data, dataset_name=DATASET_NAME, file_name="2a subject {} temp".format(str(i + 1)), n_epochs=30)
     run_sample_model(subject_data, dataset_name=DATASET_NAME, train_test_ratio=0.8,
                      file_name="temp", n_epochs=30)   #"2a subject {} shallow no_batchnorm ttratio1".format(str(i + 1))

spectralNet.py
@@ -24,7 +24,7 @@ class SpectralNet(nn.Module):
     ):
         super(SpectralNet, self).__init__()
         self.features = nn.ModuleList([nn.Sequential(
-            nn.Conv2d(1,10, kernel_size = 3, padding = 5),
+            nn.Conv2d(1,10, kernel_size = 3, padding = 4),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Conv2d(10, 14, kernel_size=3, padding=1),
@@ -131,11 +131,11 @@ def train(epochs, train_loader, model, optimizer, loss_function, PATH = './indep
     return model
 
 
-def train_and_test(train_loader, epoch, test_loader, num_channel = 20, PATH = './independent_model.pth'):
+def train_and_test(train_loader, epoch, test_loader, num_band = 20, PATH = './independent_model.pth'):
     use_cuda = torch.cuda.is_available()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    model = SpectralNet(num_channel).cuda() if use_cuda else SpectralNet(num_channel)
+    model = SpectralNet(num_band).cuda() if use_cuda else SpectralNet(num_band)
     optimizer = optim.Adam(model.parameters(), lr=0.00001)
     loss_function = nn.CrossEntropyLoss().to(device)
 